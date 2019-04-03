980e110caf065f88db2d3a4db51476363e2e2294
SangeetManghnani <sangeet.manghnani@gmail.com>
2019-04-03 08:45:28 +0530

    adding createMAtch

--- src/assets/jss/material-dashboard-react/components/sidebarStyle.jsx
+++ src/assets/jss/material-dashboard-react/components/sidebarStyle.jsx
@@ -239,24 +239,24 @@ const sidebarStyle = theme => ({
   },
   orange: {
     backgroundColor: warningColor[0],
-    // boxShadow:
-    //   "0 12px 20px -10px rgba(" +
-    //   hexToRgb(warningColor[0]) +
-    //   ",.28), 0 4px 20px 0 rgba(" +
-    //   hexToRgb(blackColor) +
-    //   ",.12), 0 7px 8px -5px rgba(" +
-    //   hexToRgb(warningColor[0]) +
-    //   ",.2)",
+    boxShadow:
+      "0 12px 20px -10px rgba(" +
+      hexToRgb(warningColor[0]) +
+      ",.28), 0 4px 20px 0 rgba(" +
+      hexToRgb(blackColor) +
+      ",.12), 0 7px 8px -5px rgba(" +
+      hexToRgb(warningColor[0]) +
+      ",.2)",
     "&:hover": {
       backgroundColor: warningColor[0],
-      // boxShadow:
-      //   "0 12px 20px -10px rgba(" +
-      //   hexToRgb(warningColor[0]) +
-      //   ",.28), 0 4px 20px 0 rgba(" +
-      //   hexToRgb(blackColor) +
-      //   ",.12), 0 7px 8px -5px rgba(" +
-      //   hexToRgb(warningColor[0]) +
-      //   ",.2)"
+      boxShadow:
+        "0 12px 20px -10px rgba(" +
+        hexToRgb(warningColor[0]) +
+        ",.28), 0 4px 20px 0 rgba(" +
+        hexToRgb(blackColor) +
+        ",.12), 0 7px 8px -5px rgba(" +
+        hexToRgb(warningColor[0]) +
+        ",.2)"
     }
   },
   red: {
